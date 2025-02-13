1f66150ebcd220472ee037706f2bafe67686e923
Muhammad Sabbir Hossain Bin Ahmad <dustobalok@gmail.com>
2025-02-13 15:40:56 +0600

    server: add users collection setup and commented route for fetching users

--- server/index.js
+++ server/index.js
@@ -30,9 +30,25 @@ async function run() {
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+
+        // all collections
+        const database = client.db("sample_mflix");
+        const usersCollection = database.collection("users");
+
+
+        // get all users
+        // app.get(`/users`, async (req, res) => {
+        //     const cursor = usersCollection.find();
+        //     const result = await cursor.toArray();
+
+        //     res.send(result);
+        // })
+
+
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
