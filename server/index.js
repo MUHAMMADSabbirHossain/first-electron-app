0a82b3f9c2d495e9b556ee860821a9e4ece95848
Muhammad Sabbir Hossain Bin Ahmad <dustobalok@gmail.com>
2025-02-13 14:33:34 +0600

    server: update root route response for user-auth testing and switch to nodemon for development

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 5000 || process.env.PORT;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello World for testing user-auth server!');
 });
 
 app.listen(PORT, () => {
